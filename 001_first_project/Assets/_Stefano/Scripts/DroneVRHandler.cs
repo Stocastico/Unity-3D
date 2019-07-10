997772c1105945fe27e38919730257f955f0bb2b
Stefano Masneri <stocastico@gmail.com>
2019-07-10 12:16:33 +0200

    work on project 002

---
+++ 001_first_project/Assets/_Stefano/Scripts/DroneVRHandler.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class DroneVRHandler : MonoBehaviour
+{
+    public Transform target;
+    private float speed = 1.5f;
+    private float step;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        transform.LookAt(target);
+
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.tag == "barrels" && target != hit.transform)
+            {
+                target = hit.transform;
+            }
+        }
+
+        if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            speed *= 2;
+            if (speed > 6)
+                speed = 1.5f;
+        }
+    }
+}
---
+++ 001_first_project/Assets/_Stefano/Scripts/RotateCube.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCube : MonoBehaviour
+{
+    public Texture2D[] textures;
+    //private int count = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Renderer>().material.mainTexture = textures[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 2, 1));
+
+        // count++;
+        // if (count == 50)
+        // {
+        //     GetComponent<Renderer>().material.mainTexture = textures[1];
+        // }
+        // else if (count == 100)
+        // {
+        //     count = 0;
+        //     GetComponent<Renderer>().material.mainTexture = textures[0];
+        // }
+    }
+
+    public void ChangeImage(int val) // Max 1 param in order to use it in Unity
+    {
+        GetComponent<Renderer>().material.mainTexture = textures[val];
+    }
+}
